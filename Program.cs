7fb16df05c3878573cce6ec4b1834e974e67c6b7
Juan <mar82697@gmail.com>
2026-03-17 21:35:21 -0500

    creacion de rutas/metodos

---
+++ Program.cs
@@ -0,0 +1,96 @@
+//using System.IO.Pipelines;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// 1. SERVICIOS
+// herramientas de uso para la api
+builder.Services.AddOpenApi();
+//configuracion para que react pida cosas
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("NuevaPolitica", policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// 2. MIDDLEWARE
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+//obliga la conexion segura https
+app.UseHttpsRedirection();
+//activa las configuraciones para react y debe ir antes que las rutas
+app.UseCors("NuevaPolitica");
+
+// 3. RUTAS
+var usuarios = new List<Usuario> { new Usuario(1, "1234","raul","zapata", new DateTime(1999, 12, 10), "3502657845","calle 24c #34-33")};
+
+//creacion de rutas
+app.MapGet("/api/usuarios", () => usuarios);
+
+app.MapPost("/api/usuarios", (Usuario nuevousuario) =>
+{
+    var hoy = DateTime.Today;
+
+    if (nuevousuario.FechaNacimiento > hoy)
+    {
+        return Results.BadRequest(new { mensaje = "La fecha de nacimiento no puede ser en el futuro." });
+    }
+    if (nuevousuario.FechaNacimiento < hoy.AddYears(-120))
+    {
+        return Results.BadRequest(new { mensaje = "La fecha de nacimiento no es válida (demasiado antigua)." });
+    }
+    int nuevoId = usuarios.Any() ? usuarios.Max(u => u.Id) + 1 : 1;
+    var Usuario = nuevousuario with { Id = usuarios.Count + 1 };
+    usuarios.Add(Usuario);
+    return Results.Ok(Usuario);
+});
+
+app.MapDelete("/api/usuarios/{id}", (int id) =>
+{
+    var UsuarioEliminado = usuarios.FirstOrDefault(u => u.Id == id);
+    if (UsuarioEliminado == null) return Results.NotFound();
+    usuarios.Remove(UsuarioEliminado);
+    return Results.Ok(new { mensaje = "Usuario eliminado", id });
+});
+
+app.MapPut("/api/usuarios/{id}", (int id, Usuario usuarioeditado) =>
+{
+    var index = usuarios.FindIndex(u => u.Id == id);
+    if (index == -1) return Results.NotFound();
+
+    usuarios[index] = usuarioeditado with { Id = id };
+    return Results.Ok(usuarios[index]);
+});
+
+
+// esta linea hace que el servidor pueda arrancar
+app.Run();
+
+public record Usuario(
+    int Id,
+    string Cedula,
+    string Nombre,
+    string Apellidos,
+    DateTime FechaNacimiento,
+    string Telefono,
+    string Direccion
+)
+{
+    
+    public int Edad
+    {
+        get
+        {
+            var hoy = DateTime.Today;
+            var edad = hoy.Year - FechaNacimiento.Year;
+            if (FechaNacimiento.Date > hoy.AddYears(-edad)) edad--;
+            return edad;
+        }
+    }
+}
